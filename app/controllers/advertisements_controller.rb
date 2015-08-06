9d2bcb3df4150d6029cb92eef3b2264329fcecad
Melanie Eitel <me@melanieeitel.com>
2015-08-05 22:24:48 -0400

    checkpoint-38-associations

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,8 +1,11 @@
 class AdvertisementsController < ApplicationController
+  
+  #localhost/advertisements
   def index
     @advertisements = Advertisement.all
   end
 
+  #localhost/advertisements/234
   def show
     @advertisement = Advertisement.find(params[:id])
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,33 @@
 require 'faker'
 
+
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
+
+
+
 #create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
     )
@@ -12,6 +37,8 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
+         # user: users.sample,   # we have not yet associated Users with Comments
+
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
@@ -26,6 +53,13 @@ end
   )
 end
 
+
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'meitel1551@gmail.com',
+   password: 'password'
+ )
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
