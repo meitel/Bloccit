35e2db4c979f155d0bf0f021550793512b99e359
Melanie Eitel <me@melanieeitel.com>
2015-08-10 21:57:59 -0400

    checkpoint-39-authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+
+  def admin?
+    role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,34 @@ require 'faker'
  
  # The `save` method then saves this User to the database.
 
+# Create an admin user
+admin = User.new(
+ name:     'Admin User',
+ email:    'admin@example.com',
+ password: 'helloworld',
+ role:     'admin'
+)
+admin.skip_confirmation!
+admin.save!
 
+# Create a moderator
+moderator = User.new(
+name:     'Moderator User',
+email:    'moderator@example.com',
+password: 'helloworld',
+role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save!
+
+# Create a member
+member = User.new(
+name:     'Member User',
+email:    'member@example.com',
+password: 'helloworld'
+)
+member.skip_confirmation!
+member.save!
 
 
 #create Posts
@@ -60,6 +87,8 @@ end
    email: 'meitel1551@gmail.com',
    password: 'password'
  )
+
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
